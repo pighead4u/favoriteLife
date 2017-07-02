74bf49cb975262879f78165ab890ed1175a5b1dc
serendipity4u <zhuhuanhuan@qccr.com>
2017-07-02 11:41:17 +0800

    init project

---
+++ app/src/main/java/com/pansijing/douban/Main.java
@@ -0,0 +1,16 @@
+package com.pansijing.douban;
+
+/**
+ * @author: holiday
+ * @date: 2017/7/2
+ * @email: zhuhuanhuan@qccr.com
+ * @desc:
+ * @version: 1.0.0
+ */
+public class Main {
+
+  public static void main(String[] args) {
+
+    System.out.println("Pansijing");
+  }
+}
---
+++ common/src/main/java/com/pansijing/common/DouBanRetrofit.java
@@ -0,0 +1,12 @@
+package com.pansijing.common;
+
+/**
+ * @author: zhuhuanhuan
+ * @time: 2017/7/2-上午11:35.
+ * @email: zhuhuanhuan@mistong.com
+ * @desc:
+ */
+
+public final class DouBanRetrofit {
+
+}
