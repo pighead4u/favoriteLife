dc7a4b71dfa3f064336583f5599da59658999627
serendipity4u <zhuhuanhuan@qccr.com>
2017-07-08 07:32:29 +0800

    add:新的依赖

--- common/src/main/java/com/pansijing/common/DouBanRetrofit.java
+++ common/src/main/java/com/pansijing/common/DouBanRetrofit.java
@@ -1,12 +1,25 @@
 package com.pansijing.common;
 
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * @author: zhuhuanhuan
  * @time: 2017/7/2-上午11:35.
  * @email: zhuhuanhuan@mistong.com
- * @desc:
+ * @desc: 用于获取豆瓣的电影和书籍
  */
 
 public final class DouBanRetrofit {
 
+  private Retrofit createRetrofit() {
+    Retrofit commonRetrofit =
+        new Retrofit.Builder().addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+            .baseUrl("http://api.douban.com")
+            .build();
+
+    return commonRetrofit;
+  }
 }
