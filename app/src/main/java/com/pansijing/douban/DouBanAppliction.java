b535533f6edfb51b1faa7a813fe8ed976b7fc4e1
serendipity4u <zhuhuanhuan@qccr.com>
2017-07-09 07:43:19 +0800

    modify:项目结构

---
+++ app/src/main/java/com/pansijing/douban/DouBanAppliction.java
@@ -0,0 +1,17 @@
+package com.pansijing.douban;
+
+import android.app.Application;
+
+/**
+ * @author: zhuhuanhuan
+ * @time: 2017/7/9-上午7:33.
+ * @email: zhuhuanhuan@mistong.com
+ * @desc:
+ */
+
+public class DouBanAppliction extends Application {
+
+  @Override public void onCreate() {
+    super.onCreate();
+  }
+}
