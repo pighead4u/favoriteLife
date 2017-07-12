7d210f18009772b5b09ff04ba7af57ce5384e0ab
serendipity4u <zhuhuanhuan@qccr.com>
2017-07-12 17:47:24 +0800

    add:new file

---
+++ common/src/main/java/com/pansijing/common/ZhiHuRetrofit.java
@@ -0,0 +1,11 @@
+package com.pansijing.common;
+
+/**
+ * @author: zhuhuanhuan
+ * @time: 2017/7/2-上午11:43.
+ * @email: zhuhuanhuan@mistong.com
+ * @desc: 用于获取知乎的专栏
+ */
+
+public class ZhiHuRetrofit {
+}
